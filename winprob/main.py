658a4d44269d9cd1bc88775de99598a65216006a
Andrew Reeves <atreeves0821@gmail.com>
2023-06-14 16:06:05 -0400

    updated predict to take in number of laps to predict on

--- winprob/main.py
+++ winprob/main.py
@@ -49,7 +49,7 @@ def main():
     print(y_final.shape)
     print(X_final)
 
-    train = True
+    train = False
     model_name = str(year) + "races" + "_no_" + skip_race
     if train:
         # define patience used for early stopping and initialize early stopping / best model saving
@@ -106,16 +106,25 @@ def main():
         p_qdf = pd.read_csv('data/'+str(p_year)+'/quali/'+str(p_year)+'_'+p_track+'_Q.csv')
 
         Xp, yp, yp_win = create_dataset(p_df, p_qdf)
+        print(Xp[0])
+        print(yp[0])
+        print(yp_win[0])
 
         # need to load model from file
         pred_model = load_model('best_models/2022races_no_'+p_track+'.h5')
 
-        pred_model.evaluate(Xp, yp)
-        pred_model.evaluate(Xp, yp_win)
+        #number of laps to predict with -- 1 = quali data --> len() = full race
+        pred_laps = 10
+        # -- could add specific lap to predict on (ex. lap 25/50 instead of full first 25 laps)
+        if pred_laps > len(Xp):
+            print("ERROR - input laps must be less than ", len(Xp))
+        else:
+            pred_model.evaluate([Xp[:pred_laps]], [yp[:pred_laps]])
+            pred_model.evaluate([Xp[:pred_laps]], [yp_win[:pred_laps]])
 
-        predicted = pred_model.predict(Xp)
-        pred = np.argmax(predicted, axis=1)
-        print(pred[:20])
+            predicted = pred_model.predict(Xp[:pred_laps])
+            pred = np.argmax(predicted, axis=1)
+            print(pred)
 
 
 if __name__ == '__main__':
